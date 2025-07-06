042715ca945d003c3967115b39b1d630b31dd801
andyochieng <andyochieng18@gmail.com>
2025-07-06 20:24:28 +0300

    aol yawa

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', include('listings.urls')),
 ]
